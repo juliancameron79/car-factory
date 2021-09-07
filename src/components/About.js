b5d4f778176d849cbb9ea23313b80c9fa2ec0868
juliancameron79 <juliancameron79@gmail.com>
2021-09-07 22:43:51 +0200

    Basic form layout setup

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+
+const Styles = styled.div`
+  .w {
+    padding-top: 5rem;
+  }
+`;
 
 export const About = () => {
   return (
-    <div>
-      <h2>What's about page up!</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione natus, laboriosam officiis porro necessitatibus quo.</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur, porro. Obcaecati, dolor tempore quos excepturi, iste nostrum quibusdam mollitia aperiam, non repudiandae magnam.</p>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
-    </div>
+    <Styles>
+      <div className="w">
+        <h2>What's about page up!</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione natus, laboriosam officiis porro necessitatibus quo.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur, porro. Obcaecati, dolor tempore quos excepturi, iste nostrum quibusdam mollitia aperiam, non repudiandae magnam.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+      </div>
+    </Styles>
   );
 };
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,35 @@
 import React from "react";
+import { Form, Button, Container, Row } from "react-bootstrap";
+import styled from "styled-components";
 
+const Styles = styled.div`
+  .w {
+    padding-top: 5rem;
+  }
+`;
 export const Contact = () => {
   return (
-    <div>
-      <h2>What's up and Contact Us!</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione natus, laboriosam officiis porro necessitatibus quo.</p>
-    </div>
+    <Styles>
+      <div className="w">
+        <Container>
+          <Row>
+            <div className="display-2">Contact Us</div>
+          </Row>
+        </Container>
+        <Form className="mt-5">
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="name@example.com" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Example textarea</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    </Styles>
   );
 };
